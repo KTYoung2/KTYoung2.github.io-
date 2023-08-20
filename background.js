82ca32b5f05ca74e0265e1158d93a2f5512b6227
KTYoung2 <115051737+KTYoung2@users.noreply.github.com>
2023-08-20 23:02:26 +0900

    Update background.js

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ const randomImg = images[Math.floor(Math.random() * images.length)];
  (.createElement)-> html에서 사용할 tag 생성 **블로그 정리할 때 좀더 알아볼것.  */
 const bgImg = document.createElement("img");
 
-bgImg.src = `/Study/img/${randomImg}`;
+bgImg.src = `./${randomImg}`;
 
 document.body.appendChild(bgImg);
 //append -> 가장 끝에 설정 prepend -> 가장 위쪽에 설정
